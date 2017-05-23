3c23d8e3a123690ae406221b629de546e7328e90
Daniel Walton <dwalton76@gmail.com>
2017-05-23 17:06:41 -0400

    2x2x2 use lookup table

--- rubikscubennnsolver/RubiksCube222.py
+++ rubikscubennnsolver/RubiksCube222.py
@@ -26,7 +26,7 @@ class RubiksCube222(RubiksCube):
     def phase(self):
         return 'Solve 2x2x2'
 
-    def solve(self):
+    def solve_non_table(self):
         """
         100% of the credit for this 2x2x2 solver goes to
         http://codegolf.stackexchange.com/questions/35002/solve-the-rubiks-pocket-cube
@@ -146,110 +146,24 @@ class RubiksCube222(RubiksCube):
 
         raise SolveError("Could not find a solution")
 
-    def solve_WIP(self):
+    def solve(self):
         '''
         lookup-table-2x2x2-solve.txt
         ============================
-        Solve 2x2x2 using only U, U', U2, B, B', B2, R, R' and R2
+        1 steps has 18 entries (0 percent, 0.00x previous step)
+        2 steps has 244 entries (0 percent, 13.56x previous step)
+        3 steps has 2,874 entries (0 percent, 11.78x previous step)
+        4 steps has 28,000 entries (0 percent, 9.74x previous step)
+        5 steps has 205,416 entries (0 percent, 7.34x previous step)
+        6 steps has 1,168,516 entries (1 percent, 5.69x previous step)
+        7 steps has 5,402,628 entries (6 percent, 4.62x previous step)
+        8 steps has 20,776,176 entries (23 percent, 3.85x previous step)
+        9 steps has 45,391,616 entries (51 percent, 2.18x previous step)
+        10 steps has 15,139,616 entries (17 percent, 0.33x previous step)
+        11 steps has 64,736 entries (0 percent, 0.00x previous step)
 
-        1 steps has 9 entries (0 percent)
-        2 steps has 55 entries (0 percent)
-        3 steps has 321 entries (0 percent)
-        4 steps has 1,847 entries (0 percent)
-        5 steps has 9,992 entries (0 percent)
-        6 steps has 50,136 entries (1 percent)
-        7 steps has 227,536 entries (6 percent)
-        8 steps has 870,072 entries (23 percent)
-        9 steps has 1,887,748 entries (51 percent)
-        10 steps has 623,800 entries (16 percent)
-        11 steps has 2,644 entries (0 percent)
-
-        Total: 3,674,160 entries
+        Total: 88,179,840 entries
         '''
         self.lt = LookupTable(self, 'lookup-table-2x2x2-solve.txt', 'all', 'UUUULLLLFFFFRRRRBBBBDDDD')
-
-        '''
-        lookup-table-2x2x2-solve.txt was constructed using only:
-            U, U', U2, B, B', B2, R, R' and R2
-
-        so we must rotate the LFD corner to be at LFD. Rotating doesn't add
-        to our move count as they are compressed out in compress_solution().
-        '''
-        if self.state[16] == 'L' and self.state[19] == 'F' and self.state[24] == 'D':
-            self.rotate_y()
-            self.rotate_y()
-        elif self.state[24] == 'L' and self.state[16] == 'F' and self.state[19] == 'D':
-            self.rotate_x()
-            self.rotate_y()
-        elif self.state[23] == 'L' and self.state[20] == 'F' and self.state[7] == 'D':
-            self.rotate_x()
-            self.rotate_z_reverse()
-            self.rotate_y()
-        elif self.state[1] == 'L' and self.state[5] == 'F' and self.state[18] == 'D':
-            self.rotate_x()
-            self.rotate_y_reverse()
-        elif self.state[15] == 'L' and self.state[22] == 'F' and self.state[12] == 'D':
-            self.rotate_y()
-            self.rotate_x()
-            self.rotate_z_reverse()
-        elif self.state[6] == 'L' and self.state[3] == 'F' and self.state[9] == 'D':
-            self.rotate_x_reverse()
-        elif self.state[11] == 'L' and self.state[21] == 'F' and self.state[8] == 'D':
-            self.rotate_x()
-            self.rotate_z_reverse()
-        elif self.state[17] == 'L' and self.state[14] == 'F' and self.state[2] == 'D':
-            self.rotate_x()
-            self.rotate_x()
-            self.rotate_y()
-        elif self.state[5] == 'L' and self.state[18] == 'F' and self.state[1] == 'D':
-            self.rotate_x()
-            self.rotate_x()
-        elif self.state[10] == 'L' and self.state[4] == 'F' and self.state[13] == 'D':
-            self.rotate_z()
-            self.rotate_y()
-        elif self.state[4] == 'L' and self.state[13] == 'F' and self.state[10] == 'D':
-            self.rotate_x_reverse()
-            self.rotate_y()
-        elif self.state[14] == 'L' and self.state[2] == 'F' and self.state[17] == 'D':
-            self.rotate_x()
-            self.rotate_y()
-            self.rotate_y()
-        elif self.state[22] == 'L' and self.state[12] == 'F' and self.state[15] == 'D':
-            self.rotate_z()
-        elif self.state[8] == 'L' and self.state[11] == 'F' and self.state[21] == 'D':
-            pass
-        elif self.state[3] == 'L' and self.state[9] == 'F' and self.state[6] == 'D':
-            self.rotate_z_reverse()
-        elif self.state[12] == 'L' and self.state[15] == 'F' and self.state[22] == 'D':
-            self.rotate_y()
-        elif self.state[2] == 'L' and self.state[17] == 'F' and self.state[14] == 'D':
-            self.rotate_z()
-            self.rotate_y()
-            self.rotate_y()
-        elif self.state[13] == 'L' and self.state[10] == 'F' and self.state[4] == 'D':
-            self.rotate_z()
-            self.rotate_z()
-        elif self.state[9] == 'L' and self.state[6] == 'F' and self.state[3] == 'D':
-            self.rotate_z()
-            self.rotate_z()
-            self.rotate_y()
-        elif self.state[21] == 'L' and self.state[8] == 'F' and self.state[11] == 'D':
-            self.rotate_x_reverse()
-            self.rotate_y_reverse()
-        elif self.state[19] == 'L' and self.state[24] == 'F' and self.state[16] == 'D':
-            self.rotate_x()
-            self.rotate_z()
-        elif self.state[18] == 'L' and self.state[1] == 'F' and self.state[5] == 'D':
-            self.rotate_y_reverse()
-            self.rotate_x_reverse()
-        elif self.state[20] == 'L' and self.state[7] == 'F' and self.state[23] == 'D':
-            self.rotate_y_reverse()
-        elif self.state[7] == 'L' and self.state[23] == 'F' and self.state[20] == 'D':
-            self.rotate_x()
-        else:
-            raise SolveError("Implement this")
-
-        # lt is a LookupTable object for lookup-table-2x2x2-solve.txt
-        self.print_cube()
         self.lt.solve()
         self.compress_solution()

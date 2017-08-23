3d017b1a348312e89d90d4ec29ae45305ec16491
Daniel Walton <dwalton76@gmail.com>
2017-08-22 21:35:33 -0400

    Removed lt_ULFRBD_centers_solve_unstaged experiment

--- rubikscubennnsolver/RubiksCube444.py
+++ rubikscubennnsolver/RubiksCube444.py
@@ -113,7 +113,6 @@ class RubiksCube444(RubiksCube):
     def __init__(self, state, order, colormap=None, avoid_pll=True, debug=False):
         RubiksCube.__init__(self, state, order, colormap, debug)
         self.avoid_pll = avoid_pll
-        self.experiment = False
 
         if debug:
             log.setLevel(logging.DEBUG)
@@ -324,243 +323,33 @@ class RubiksCube444(RubiksCube):
             return
         self.lt_init_called = True
 
-        # solve all centers without staging
-        if self.experiment:
-            self.lt_UD_centers_solve = LookupTable(self,
-                                                   'lookup-table-4x4x4-step101-UD-centers-solve.txt',
-                                                   '444-UD-centers-solve',
-                                                   'UUUUxxxxxxxxxxxxxxxxDDDD',
-                                                    False, # state hex
-                                                    modulo=51482970)
+        self.lt_init_common()
 
-            self.lt_LR_centers_solve = LookupTable(self,
-                                                   'lookup-table-4x4x4-step102-LR-centers-solve.txt',
-                                                   '444-LR-centers-solve',
-                                                   'xxxxLLLLxxxxRRRRxxxxxxxx',
-                                                    False, # state hex
-                                                    modulo=51482970)
+        '''
+        (8!/(4! * 4!))^3 is 343,000
 
-            self.lt_FB_centers_solve = LookupTable(self,
-                                                   'lookup-table-4x4x4-step103-FB-centers-solve.txt',
-                                                   '444-FB-centers-solve',
-                                                   'xxxxxxxxFFFFxxxxBBBBxxxx',
-                                                    False, # state hex
-                                                    modulo=51482970)
+        lookup-table-4x4x4-step03-ULFRBD-centers-solve.txt
+        ==================================================
+        1 steps has 7 entries (0 percent)
+        2 steps has 99 entries (0 percent)
+        3 steps has 996 entries (0 percent)
+        4 steps has 6,477 entries (1 percent)
+        5 steps has 23,540 entries (6 percent)
+        6 steps has 53,537 entries (15 percent)
+        7 steps has 86,464 entries (25 percent)
+        8 steps has 83,240 entries (24 percent)
+        9 steps has 54,592 entries (15 percent)
+        10 steps has 29,568 entries (8 percent)
+        11 steps has 4,480 entries (1 percent)
 
-            self.lt_ULFRBD_centers_solve_unstaged = LookupTableIDA(self,
-                                                   'lookup-table-4x4x4-step100-ULFRBD-centers-solve-unstaged.txt',
+        Total: 343,000 entries
+        '''
+        self.lt_ULFRBD_centers_solve = LookupTable(self,
+                                                   'lookup-table-4x4x4-step30-ULFRBD-centers-solve.txt',
                                                    '444-ULFRBD-centers-solve',
                                                    'UUUULLLLFFFFRRRRBBBBDDDD',
-                                                    False, # state hex
-                                                    moves_4x4x4,
-                                                    (), # illegal_moves
-
-                                                    # prune tables
-                                                    (self.lt_UD_centers_solve,
-                                                     self.lt_LR_centers_solve,
-                                                     self.lt_FB_centers_solve),
-                                                    modulo=87727430)
-
-            self.lt_ULFRBD_centers_solve_unstaged.heuristic_stats = {
-                (0, 0, 2) : 3,
-                (0, 1, 1) : 1,
-                (0, 1, 2) : 2,
-                (0, 2, 1) : 2,
-                (0, 2, 2) : 2,
-                (0, 3, 3) : 3,
-                (1, 0, 1) : 1,
-                (1, 0, 2) : 2,
-                (1, 1, 0) : 1,
-                (1, 1, 1) : 4,
-                (1, 1, 2) : 4,
-                (1, 2, 0) : 2,
-                (1, 2, 1) : 3,
-                (1, 2, 2) : 2,
-                (1, 2, 3) : 3,
-                (1, 3, 2) : 3,
-                (1, 3, 3) : 3,
-                (1, 3, 4) : 4,
-                (1, 4, 4) : 4,
-                (2, 0, 1) : 2,
-                (2, 0, 2) : 3,
-                (2, 1, 0) : 2,
-                (2, 1, 1) : 3,
-                (2, 1, 2) : 2,
-                (2, 1, 3) : 4,
-                (2, 2, 0) : 2,
-                (2, 2, 1) : 2,
-                (2, 2, 2) : 4,
-                (2, 2, 3) : 5,
-                (2, 2, 4) : 4,
-                (2, 3, 1) : 3,
-                (2, 3, 2) : 5,
-                (2, 3, 3) : 4,
-                (2, 3, 4) : 5,
-                (2, 3, 5) : 5,
-                (2, 4, 2) : 4,
-                (2, 4, 4) : 5,
-                (2, 5, 5) : 6,
-                (3, 0, 0) : 3,
-                (3, 0, 1) : 3,
-                (3, 0, 2) : 3,
-                (3, 0, 3) : 4,
-                (3, 1, 1) : 4,
-                (3, 1, 2) : 4,
-                (3, 1, 3) : 3,
-                (3, 1, 4) : 5,
-                (3, 2, 1) : 3,
-                (3, 2, 2) : 4,
-                (3, 2, 3) : 3,
-                (3, 2, 4) : 6,
-                (3, 3, 0) : 3,
-                (3, 3, 1) : 3,
-                (3, 3, 2) : 4,
-                (3, 3, 3) : 6,
-                (3, 3, 4) : 6,
-                (3, 4, 1) : 4,
-                (3, 4, 2) : 4,
-                (3, 4, 3) : 6,
-                (3, 4, 4) : 7,
-                (3, 4, 5) : 5,
-                (3, 5, 3) : 5,
-                (3, 5, 4) : 6,
-                (3, 5, 5) : 8,
-                (3, 6, 4) : 9,
-                (3, 7, 7) : 9,
-                (4, 0, 4) : 4,
-                (4, 1, 1) : 4,
-                (4, 1, 2) : 4,
-                (4, 1, 3) : 4,
-                (4, 1, 4) : 4,
-                (4, 2, 2) : 5,
-                (4, 2, 3) : 5,
-                (4, 2, 4) : 6,
-                (4, 3, 2) : 6,
-                (4, 3, 3) : 5,
-                (4, 3, 4) : 6,
-                (4, 3, 5) : 8,
-                (4, 4, 1) : 4,
-                (4, 4, 2) : 6,
-                (4, 4, 3) : 7,
-                (4, 4, 4) : 7,
-                (4, 4, 5) : 8,
-                (4, 5, 3) : 5,
-                (4, 5, 4) : 8,
-                (4, 5, 5) : 6,
-                (4, 5, 6) : 6,
-                (4, 6, 5) : 7,
-                (4, 6, 6) : 9,
-                (4, 7, 5) : 10,
-                (4, 8, 8) : 10,
-                (5, 2, 3) : 5,
-                (5, 2, 5) : 7,
-                (5, 3, 3) : 5,
-                (5, 3, 4) : 6,
-                (5, 3, 5) : 7,
-                (5, 4, 3) : 5,
-                (5, 4, 4) : 6,
-                (5, 4, 5) : 7,
-                (5, 4, 6) : 9,
-                (5, 5, 3) : 8,
-                (5, 5, 4) : 7,
-                (5, 5, 5) : 8,
-                (5, 5, 6) : 8,
-                (5, 6, 3) : 9,
-                (5, 6, 4) : 6,
-                (5, 6, 5) : 9,
-                (5, 6, 6) : 8,
-                (5, 7, 4) : 10,
-                (5, 7, 6) : 10,
-                (5, 7, 7) : 12,
-                (5, 8, 7) : 11,
-                (6, 3, 6) : 8,
-                (6, 4, 5) : 8,
-                (6, 4, 6) : 9,
-                (6, 4, 7) : 10,
-                (6, 5, 4) : 7,
-                (6, 5, 5) : 8,
-                (6, 5, 6) : 10,
-                (6, 5, 7) : 9,
-                (6, 6, 3) : 9,
-                (6, 6, 4) : 9,
-                (6, 6, 5) : 9,
-                (6, 6, 6) : 9,
-                (6, 6, 7) : 11,
-                (6, 6, 8) : 9,
-                (6, 7, 4) : 10,
-                (6, 7, 5) : 10,
-                (6, 7, 6) : 10,
-                (6, 7, 7) : 11,
-                (6, 7, 8) : 12,
-                (6, 8, 5) : 11,
-                (6, 8, 6) : 11,
-                (6, 8, 7) : 12,
-                (6, 8, 8) : 11,
-                (7, 4, 6) : 9,
-                (7, 5, 6) : 9,
-                (7, 5, 7) : 11,
-                (7, 5, 8) : 11,
-                (7, 6, 4) : 11,
-                (7, 6, 5) : 10,
-                (7, 6, 6) : 11,
-                (7, 6, 7) : 11,
-                (7, 6, 8) : 12,
-                (7, 7, 4) : 10,
-                (7, 7, 5) : 10,
-                (7, 7, 6) : 12,
-                (7, 7, 7) : 12,
-                (7, 7, 8) : 13,
-                (7, 8, 5) : 11,
-                (7, 8, 6) : 13,
-                (7, 8, 7) : 13,
-                (7, 8, 8) : 15,
-                (8, 5, 6) : 12,
-                (8, 5, 7) : 11,
-                (8, 6, 6) : 11,
-                (8, 6, 7) : 12,
-                (8, 6, 8) : 12,
-                (8, 7, 5) : 11,
-                (8, 7, 6) : 12,
-                (8, 7, 7) : 13,
-                (8, 7, 8) : 13,
-                (8, 8, 5) : 15,
-                (8, 8, 6) : 14,
-                (8, 8, 7) : 14,
-                (8, 8, 8) : 14
-            }
-            # TODO remove this once we have collected/crunch stats
-            # and uesd that to update the numbers above
-            self.lt_ULFRBD_centers_solve_unstaged.heuristic_stats = {}
-
-        # normal way
-        else:
-            self.lt_init_common()
-
-            '''
-            (8!/(4! * 4!))^3 is 343,000
-
-            lookup-table-4x4x4-step03-ULFRBD-centers-solve.txt
-            ==================================================
-            1 steps has 7 entries (0 percent)
-            2 steps has 99 entries (0 percent)
-            3 steps has 996 entries (0 percent)
-            4 steps has 6,477 entries (1 percent)
-            5 steps has 23,540 entries (6 percent)
-            6 steps has 53,537 entries (15 percent)
-            7 steps has 86,464 entries (25 percent)
-            8 steps has 83,240 entries (24 percent)
-            9 steps has 54,592 entries (15 percent)
-            10 steps has 29,568 entries (8 percent)
-            11 steps has 4,480 entries (1 percent)
-
-            Total: 343,000 entries
-            '''
-            self.lt_ULFRBD_centers_solve = LookupTable(self,
-                                                       'lookup-table-4x4x4-step30-ULFRBD-centers-solve.txt',
-                                                       '444-ULFRBD-centers-solve',
-                                                       'UUUULLLLFFFFRRRRBBBBDDDD',
-                                                       False, # state hex
-                                                       modulo=343000)
+                                                   False, # state hex
+                                                   modulo=343000)
 
 
         '''
@@ -635,43 +424,19 @@ class RubiksCube444(RubiksCube):
 
     def group_centers_guts(self):
         self.lt_init()
+        self.lt_ULFRBD_centers_stage.solve()
 
-        # solve all centers without staging
-        if self.experiment:
-            # Test prune tables
-            #self.lt_UD_centers_solve.solve()
-            #self.lt_LR_centers_solve.solve()
-            #self.lt_FB_centers_solve.solve()
-            #self.print_cube()
-            #sys.exit(0)
+        # Made some pics to try to explain lookup tables on facebook
+        #
+        #self.print_cube()
+        #for side in (self.sideU, self.sideL, self.sideF, self.sideR, self.sideB, self.sideD):
+        #    for square in side.edge_pos:
+        #        self.state[square] = 'x'
+        #    for square in side.corner_pos:
+        #        self.state[square] = 'x'
+        #self.print_cube()
 
-            # TODO
-            self.lt_ULFRBD_centers_solve_unstaged.avoid_oll = True
-            self.lt_ULFRBD_centers_solve_unstaged.record_stats = True
-            self.lt_ULFRBD_centers_solve_unstaged.solve()
-
-            # Test prune tables
-            #self.lt_UD_centers_stage.solve()
-            #self.lt_LR_centers_stage.solve()
-            #self.lt_FB_centers_stage.solve()
-            #self.print_cube()
-            #sys.exit(0)
-
-        # normal way
-        else:
-            self.lt_ULFRBD_centers_stage.solve()
-
-            # Made some pics to try to explain lookup tables on facebook
-            #
-            #self.print_cube()
-            #for side in (self.sideU, self.sideL, self.sideF, self.sideR, self.sideB, self.sideD):
-            #    for square in side.edge_pos:
-            #        self.state[square] = 'x'
-            #    for square in side.corner_pos:
-            #        self.state[square] = 'x'
-            #self.print_cube()
-
-            self.lt_ULFRBD_centers_solve.solve()
+        self.lt_ULFRBD_centers_solve.solve()
 
     def edge_string_to_find(self, target_wing, sister_wing1, sister_wing2, sister_wing3):
         state = []

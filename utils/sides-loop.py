3b878c3983217a3d0b80cf6b18f1e3ff7ed4f775
Daniel Walton <dwalton76@gmail.com>
2018-10-08 06:50:16 -0400

    binary_search_multiple perf improvements

--- utils/sides-loop.py
+++ utils/sides-loop.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-foo = [33, 37, 38, 39, 43]
+foo = [8, 12, 13, 14, 18]
 offset = 0
 side_name = {
     0 : "Upper",
@@ -11,6 +11,7 @@ side_name = {
     5 : "Down",
 }
 
-for side_index in range(1,5):
+#for side_index in range(1,5):
+for side_index in range(6):
     print("        %s, # %s" % (', '.join([str(x + offset) for x in foo]), side_name[side_index]))
     offset += 25
